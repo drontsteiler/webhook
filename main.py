f7f0136d545520662914b643af173b4d1da18bb9
foo <bar@example.com>
2018-07-17 18:32:10 +0300

    Add flask

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import os
-import telebot
 
-TOKEN = "683488884:AAFpyXXOeIBnGaMPfWfvcO0bUUYKv4CPW8s"
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-##########################################
+import telebot
+from flask import Flask, request
+
+TOKEN = '683488884:AAFpyXXOeIBnGaMPfWfvcO0bUUYKv4CPW8s'
 bot = telebot.TeleBot(TOKEN)
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, "You write to me: "+message.text)
-    print ("Message text: "+ message.text)
-    
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
-###############################################
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://telegram-bot-r.herokuapp.com/" + TOKEN)
-updater.idle()
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://telegram-bot-r.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
